5af5277d583a492be18ba55d9902e3a076101130
xinxi <xinxi@luojilab.com>
2019-06-03 23:52:32 +0800

    first commit

---
+++ src/renderer/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+   <div>
+      <div style="margin-bottom: 20px;margin-top: 20px;margin-left: 20px;">
+          <Button type="primary"  @click="goDevicesPage()">设备连接</Button>
+      </div>
+      <div style="margin-bottom: 20px;margin-top: 20px;margin-left: 20px;">
+          <Button type="primary"  @click="goLogPage()">LOG日志</Button>
+      </div>
+      <div style="margin-bottom: 20px;margin-top: 20px;margin-left: 20px;">
+          <Button type="primary" >屏幕截图</Button>
+      </div>
+      <div style="margin-bottom: 20px;margin-top: 20px;margin-left: 20px;">
+          <Button type="primary" >视频录制</Button>
+      </div>
+      <div style="margin-bottom: 20px;margin-top: 20px;margin-left: 20px;">
+          <Button type="primary" >性能数据</Button>
+      </div>
+   </div>
+</template>
+
+<script>
+     export default {
+        data () {
+            return {
+                theme3: 'light'
+            }
+        },
+        methods: {
+            goDevicesPage() {
+                this.$router.push('/devices');
+            },
+            goLogPage() {
+                this.$router.push('/log');
+            },
+         }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import axios from 'axios'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+import App from './App'
+import Routers from './router/index.js';
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+Vue.use(iView)
+Vue.use(VueRouter);
+
+// 路由配置
+const RouterConfig = {
+    mode: 'history',
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
+
+// /* eslint-disable no-new */
+// new Vue({
+//   components: { App },
+//   router,
+//   store,
+//   template: '<App/>'
+// }).$mount('#app')
+
+
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
+
